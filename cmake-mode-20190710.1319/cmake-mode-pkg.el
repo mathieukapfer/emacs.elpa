eafeddf8a46c4164d4cad6eb253997bc07d78c89
Kapfer Family <kapfer@debian-desktop>
2020-11-04 09:06:46 +0100

    creation

---
+++ cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "af4243f054706e260f2f14f141613fb26974ae6c")
