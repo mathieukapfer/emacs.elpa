0cb0dd529025ee5af0eb92be2f24ce063e929182
Kapfer Family <kapfer@debian-desktop>
2022-06-17 07:44:08 +0200

    local elpa - next

---
+++ dynamic-graphs-20200902.1238/dynamic-graphs-autoloads.el
@@ -0,0 +1,15 @@
+;;; dynamic-graphs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dynamic-graphs-autoloads.el ends here
---
+++ dynamic-graphs-20200902.1238/dynamic-graphs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dynamic-graphs" "20200902.1238" "Manipulation with graphviz graphs" '((emacs "26.1")) :commit "ba3fdf2cf0e5e1e952a1961a03dfb7f61a4ab0e7" :authors '(("Tomas Zellerin" . "tomas@zellerin.cz")) :maintainer '("Tomas Zellerin" . "tomas@zellerin.cz") :keywords '("tools") :url "https://github.com/zellerin/dynamic-graphs")
